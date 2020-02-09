c02350edf5c1360de9f9b9e1420dd4e09eb83555
Sergei <sergej-93@mail.ru>
2020-02-09 21:11:26 +0600

    Полностью рабочий билд

--- src/main/java/com/example/ForestProtect/controllers/HelloController.java
+++ src/main/java/com/example/ForestProtect/controllers/HelloController.java
@@ -1,20 +1,36 @@
 package com.example.ForestProtect.controllers;
 
+import com.sun.javafx.util.Logging;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.Charset;
+
 @Controller
 public class HelloController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping(value = "/",  produces={"text/html; charset=UTF-8"})
-    public ModelAndView hello () {
+    private ModelAndView hello () {
         ModelAndView mv = new ModelAndView("hello");
+        LOGGER.info("My default Charset " + Charset.defaultCharset());
         return mv;
     }
     @RequestMapping(value = "/download.html")
-    public ModelAndView download(){
+    private ModelAndView download(){
         ModelAndView mv = new ModelAndView("download");
 
         return mv;
     }
+    @RequestMapping(value = "/ImageVerification.html")
+    private ModelAndView verification(){
+        ModelAndView mv = new ModelAndView("ImageVerification");
+
+        return mv;
+    }
+
 }
